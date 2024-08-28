3043a49aa92c1c1bb75dd9bc720ebd58ea589680
uthso297 <uthso119464@gmail.com>
2024-08-28 22:48:44 +0600

    Variable

---
+++ firstjavascript.js
@@ -0,0 +1,18 @@
+var name = "Sadar Uddin";
+console.log(name);
+var isRich = true;
+var isPoor = false;
+console.log(isRich);
+console.log(typeof isPoor);
+var applePrice = parseInt('900');
+var bananaPrice = parseFloat('900.5');
+console.log(applePrice);
+var orrangePrice = 90;
+console.log(applePrice + orrangePrice + bananaPrice);
+
+var first = 0.2;
+var second = 0.1;
+
+var total = first + second;
+console.log(total);
+console.log(total.toFixed(3));
