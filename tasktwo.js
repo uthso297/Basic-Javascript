f226cc4acb40b05d051fc020ab4c6442b6eb0316
uthso297 <uthso119464@gmail.com>
2024-08-28 23:47:22 +0600

    tasktwo

---
+++ tasktwo.js
@@ -0,0 +1,8 @@
+var Mathematics = 75.25;
+var Biology = 65;
+var Chemistry = 80;
+var Physics = 35.45;
+var Bangla = 99.50;
+var total = Biology + Biology + Chemistry + Physics + Bangla;
+var average = total/5;
+console.log(average);
