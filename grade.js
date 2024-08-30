072f718f01caf6e19442137ce0210e8159a31e11
uthso297 <uthso119464@gmail.com>
2024-08-30 14:18:18 +0600

    Grade

---
+++ grade.js
@@ -0,0 +1,21 @@
+let grade =79;
+
+if(grade>=90 && grade<=100){
+    console.log("Grade A");
+}
+
+else if(grade>=80 && grade<=89){
+    console.log("Grade B");
+}
+else if(grade>=80 && grade<=89){
+    console.log("Grade C");
+}
+else if(grade>=70 && grade<=79){
+    console.log("Grade D");
+}
+else if(grade>=60 && grade<=69){
+    console.log("Grade E");
+}
+else if(grade>=0 && grade<=59){
+    console.log("Grade F");
+}
